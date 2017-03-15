af3e22ad6bda78f96d80b120352be341ee94bbc2
msolimando <msolimando@DESKTOP-78UL8JM>
2017-03-15 17:39:31 +0100

    Write Positions in Files

--- CloudletServer_JClouds_MVN/src/com/example/cloudlet/ingunibo/cloudlettestapp/Positions_Thread.java
+++ CloudletServer_JClouds_MVN/src/com/example/cloudlet/ingunibo/cloudlettestapp/Positions_Thread.java
@@ -1,12 +1,18 @@
 package com.example.cloudlet.ingunibo.cloudlettestapp;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Positions_Thread extends Thread {
 	private ServerSocket cloudletServRecPositionsSock; 
@@ -18,7 +24,9 @@ public class Positions_Thread extends Thread {
     
     //TODO move configuration in right file. 
     private static final int DEF_CLOUDLET_SERVER_POSITIONS_PORT = 11111;
-    private static final int MAX_POSITIONS_NUM = 200;
+    private static final int MAX_POSITIONS_NUM = 20;
+    private static final String LAT_FILE_NAME = "latitude.txt";
+    private static final String LON_FILE_NAME = "longitude.txt";
 	
 	public Positions_Thread() {
 		super();
@@ -30,6 +38,7 @@ public class Positions_Thread extends Thread {
 			System.out.println("\nWaiting for positions update on port ...");
 			positionsReceived=null;
 			cloudletClientSock = cloudletServRecPositionsSock.accept();
+			System.out.println("Connected: "+cloudletClientSock.getRemoteSocketAddress());
 			//in = new BufferedReader(new InputStreamReader(cloudletClientSock.getInputStream()));
 			in = new ObjectInputStream(cloudletClientSock.getInputStream());
 			out = new PrintWriter(cloudletClientSock.getOutputStream());
@@ -40,16 +49,41 @@ public class Positions_Thread extends Thread {
 				e.printStackTrace();
 			}
 			
-			//DEBUG
-			System.out.println("Received positions: "+printPositions());
+			if(positionsReceived!=null){
+				//DEBUG
+				System.out.println("Received positions: "+printPositions());
+				
+				//writing positions to files
+				File latFile = new File(LAT_FILE_NAME);
+				File lonFile = new File(LON_FILE_NAME);			
+				
+				// TODO cycle all positions
+				for (int row=0;row<positionsReceived.length;row++){
+					if(latFile.exists() && !latFile.isDirectory()) { 
+					    Files.write(Paths.get(LAT_FILE_NAME), System.lineSeparator().getBytes(), StandardOpenOption.APPEND);
+					    Files.write(Paths.get(LAT_FILE_NAME), (String.valueOf(positionsReceived[row][0])+"\n").getBytes(), StandardOpenOption.APPEND);
+					}
+					else if (!latFile.exists()){
+					    Files.write(Paths.get(LAT_FILE_NAME), (String.valueOf(positionsReceived[row][0])+"\n").getBytes(), StandardOpenOption.CREATE_NEW);
+					}
+					if(lonFile.exists() && !lonFile.isDirectory()) { 
+					    Files.write(Paths.get(LON_FILE_NAME), System.lineSeparator().getBytes(), StandardOpenOption.APPEND);
+					    Files.write(Paths.get(LON_FILE_NAME), (String.valueOf(positionsReceived[row][1])+"\n").getBytes(), StandardOpenOption.APPEND);
+					}
+					else if (!lonFile.exists()){
+					    Files.write(Paths.get(LON_FILE_NAME), (String.valueOf(positionsReceived[row][1])+"\n").getBytes(), StandardOpenOption.CREATE_NEW);
+					}
+				}
+				
+				
+				//TODO: check if there are enough positions to send to cloud.
+				
+				
+				/**
+			     * curl -F userID_lat@="localpath" http://SERVER_IP:SERVER_PORT
+			     * */
+			}
 			
-			
-			
-			//TODO: append positions in file if exists, otherwise create it. 
-			//TODO: check if there are enough positions to send to cloud.
-			/**
-		     * curl -F userID_lat@="localpath" http://SERVER_IP:SERVER_PORT
-		     * */
 				
 			closeConnections();
 
@@ -57,12 +91,12 @@ public class Positions_Thread extends Thread {
 		catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("The client brutally went out!!Closing...");
-//			try {
-//				closeConnections();
-//			} catch (IOException e1) {
-//				e1.printStackTrace();
-//				System.out.println("Problem in closing connection...EXIT");
-//			}
+			try {
+				closeConnections();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+				System.out.println("Problem in closing connection...EXIT");
+			}
 		}
 
 	}
@@ -81,10 +115,9 @@ public class Positions_Thread extends Thread {
 	}
 	
 	/*TEST PRINT POSITIONS*/
-	//TODO better...
     private String printPositions(){
         String result="";
-        result+="Rows --> " + positionsReceived.length;
+        result+="Rows --> " + positionsReceived.length+"\n";
         for (int row=0;row<positionsReceived.length;row++){
             for (int col=0;col<2;col++){
             	result=result+positionsReceived[row][col]+"\n";
